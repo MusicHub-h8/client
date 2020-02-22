1def1c3491999a0554efa4f661363cc6b2318f44
Jelang R. Kharisma <jelangrkharisma@gmail.com>
2020-02-22 15:26:34 +0700

    setup redux tmplte

---
+++ src/store/actions/index.js
@@ -0,0 +1,10 @@
+import axiosInstance from '../../api/axiosInstance'
+
+export const GET_MYROOMS = 'GET_MYROOMS'
+
+// request => hit server
+// recieve => dispatch reducer
+
+export const requestRooms = () => {
+  return null
+}
---
+++ src/store/reducers/trackReducers.js
@@ -0,0 +1,17 @@
+import {} from '../actions'
+
+const initialState = {}
+
+function trackReducers(state = initialState, action) {
+  // switch (action.type) {
+  //   case NAMAACTIONGOESHERE:
+  //     return {
+  //       ...state,
+  //       states: action.payload
+  //     }
+  //   default:
+  //     return state
+  // }
+}
+
+export default trackReducers
