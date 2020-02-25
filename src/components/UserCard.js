a45a0d20355a6c714a1be044813d00bbf8953fdd
vaniairnanda <vaniairnanda@gmail.com>
2020-02-25 14:57:47 +0700

    searchBar RoomTitle

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import InviteModal from '../views/Dashboard/components/InviteModal';
+import React, { useState } from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import InviteModal from "../views/Dashboard/components/InviteModal";
 
 const UserCard = props => {
   const { player } = props;
@@ -18,26 +18,26 @@ const UserCard = props => {
   return (
     <>
       <ReactCSSTransitionGroup
-        transitionName='example'
+        transitionName="example"
         transitionEnterTimeout={500}
         transitionLeaveTimeout={300}
       >
         {inviteUser(player._id)}
       </ReactCSSTransitionGroup>
 
-      <div className='card-recommended'>
-        <div className='card-left'>
-          <img src={player.avatar} alt='Player Avatar' />
+      <div className="card-recommended">
+        <div className="card-left">
+          <img src={player.avatar} alt="Player Avatar" />
         </div>
-        <div className='card-right'>
-          <p className='card-name'>{player.display_name}</p>
+        <div className="card-right">
+          <p className="card-name">{player.display_name}</p>
           <p>{player.genre}</p>
-          <div className='card-right-bottom'>
-            {['Guitar', 'Bass', 'Piano'].map((instrument, index) => {
+          <div className="card-right-bottom">
+            {player.instruments.map((instrument, index) => {
               return (
                 <p
-                  key={'instrument' + index}
-                  style={{ marginRight: '8px', color: '#fa5a97' }}
+                  key={"instrument" + index}
+                  style={{ marginRight: "8px", color: "#fa5a97" }}
                 >
                   {instrument}
                 </p>
@@ -46,9 +46,9 @@ const UserCard = props => {
           </div>
         </div>
         <div
-          style={{ alignSelf: 'flex-end', height: '100%', marginLeft: 'auto' }}
+          style={{ alignSelf: "flex-end", height: "100%", marginLeft: "auto" }}
         >
-          <button onClick={() => handleShowModal()} className='btn-invite'>
+          <button onClick={() => handleShowModal()} className="btn-invite">
             Invite
           </button>
         </div>
