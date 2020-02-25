6f43b26526bc4e0a692430d4b08de4f5f9f51d96
vaniairnanda <vaniairnanda@gmail.com>
2020-02-25 19:54:40 +0700

    fix-live

--- src/views/JammingStudio/components/StudioHeader.js
+++ src/views/JammingStudio/components/StudioHeader.js
@@ -1,67 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-import FormAdd from './FormAdd'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import FormAdd from "./FormAdd";
 
 export default function StudioHeader(props) {
-  const [showForm, setShowForm] = useState(false)
-  const currentUser = useSelector((state) => state.userReducer.currentUser)
-
-  const players = props.roomDetail.userIds
-  const history = useHistory()
+  const [showForm, setShowForm] = useState(false);
+  // const roomOwner = props.roomDetail.roomOwner;
+  const players = props.roomDetail.userIds;
+  const history = useHistory();
 
   const handleGoBack = () => {
-    history.push('/dashboard/studios')
-  }
+    history.push("/dashboard/studios");
+  };
   const toggleForm = () => {
-    setShowForm(!showForm)
-  }
+    setShowForm(!showForm);
+  };
 
   useEffect(() => {
-    console.log(players)
-  }, [players])
+    console.log(players);
+  }, [players]);
   return (
     <>
-      <div className='studio-HeaderContainer text-white'>
-        <div className='studio-heading'>
-          <div className='leftSide'>
+      <div className="studio-HeaderContainer text-white">
+        <div className="studio-heading">
+          <div className="leftSide">
             <button
-              className='dash-add-btn mb-2 mr-2'
+              className="dash-add-btn mb-2 mr-2"
               onClick={() => {
-                handleGoBack()
+                handleGoBack();
               }}
             >
-              <i class='fas fa-chevron-left'></i>
+              <i class="fas fa-chevron-left"></i>
             </button>
             <button
-              className='dash-add-btn'
-              onClick={(event) => {
-                toggleForm()
+              className="dash-add-btn"
+              onClick={event => {
+                toggleForm();
               }}
             >
-              <i class='fas fa-plus'></i> Add Track
+              <i class="fas fa-plus"></i> Add Track
             </button>
           </div>
-          <div className='rightSide'>
-            <div className='playerAvaContainer'>
+          <div className="rightSide">
+            <div className="playerAvaContainer">
               {players &&
-                players.map((player) => (
+                players.map(player => (
                   <img
                     src={player.avatar}
                     alt={player.display_name}
-                    className='playerAva'
+                    className="playerAva"
                   />
                 ))}
-              <img
-                src={currentUser.avatar}
-                alt={currentUser.display_name}
-                className='playerAva'
-              />
+              {/* {roomOwner && (
+                <img
+                  src={roomOwner.avatar}
+                  alt={roomOwner.display_name}
+                  className="playerAva"
+                />
+              )} */}
+              {props.roomDetail.roomOwner && (
+                <img
+                  src={props.roomDetail.roomOwner.avatar}
+                  alt={props.roomDetail.roomOwner.display_name}
+                  className="playerAva"
+                />
+              )}
             </div>
           </div>
         </div>
         {showForm && <FormAdd />}
       </div>
     </>
-  )
+  );
 }
