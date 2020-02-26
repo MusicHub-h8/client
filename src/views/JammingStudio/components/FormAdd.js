3507e9bfa675b75207f12700eb16b4404e3904f7
Jelang R. Kharisma <jelangrkharisma@gmail.com>
2020-02-26 11:40:32 +0700

    send base64 insteadof file

--- src/views/JammingStudio/components/FormAdd.js
+++ src/views/JammingStudio/components/FormAdd.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import SocketIOFileUpload from 'socketio-file-upload'
+import { Base64 } from 'js-base64'
 
 import { requestAddTrack } from '../../../store/actions'
 
 export default function FormAdd() {
   const roomDetail = useSelector((state) => state.roomReducer.activeRoom.detail)
 
+  const uploader = new SocketIOFileUpload(window.socket)
+  useEffect(() => {}, [])
+
   const [instrument, setInstrument] = useState('')
   const [selectedFile, setSelectedFile] = useState({})
+  const [base64encodedFile, setBase64encodedFile] = useState()
   const dispatch = useDispatch()
 
-  const handleFileUpload = (selectedFile) => {
+  const handleFileUpload = async (selectedFile) => {
     setSelectedFile(selectedFile[0])
+    const toBase64 = (file) =>
+      new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = () => resolve(reader.result)
+        reader.onerror = (error) => reject(error)
+      })
+    const file = selectedFile[0]
+    setBase64encodedFile(await toBase64(file))
   }
   const handleInstrumentInput = (input) => {
     setInstrument(input)
   }
   const handleSubmit = (event) => {
     event.preventDefault()
+    // ORIGINAL FILEUPLOAD
     const roomId = roomDetail._id
     const trackInfo = {
       roomId,
       instrument,
-      selectedFile,
+      base64encodedFile,
     }
     dispatch(requestAddTrack(trackInfo))
   }
+
   return (
     <form
       className='form-container'
@@ -47,15 +64,16 @@ export default function FormAdd() {
             handleFileUpload(event.target.files)
           }}
           type='file'
+          id='siofu_input'
           className='custom-file-input'
-          id='customFile'
-          accept='.mp3, .mp4, .ogg, .wav, .flac'
+          accept='.mp3'
         />
         <label className='custom-file-label'>
+          {/* upload track */}
           {selectedFile.name ? selectedFile.name : 'add track'}
         </label>
       </div>
-      <input type='Submit' className='dash-add-btn' />
+      <input type='Submit' id='uploadSubmit' className='dash-add-btn' />
     </form>
   )
 }
