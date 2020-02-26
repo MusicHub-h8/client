85c14f8a1898eeac1b5a9127a8159389aab3aac0
Gabriel Timothy <biel.jaeger03@gmail.com>
2020-02-26 15:00:30 +0700

    add Track is now live

--- src/views/JammingStudio/index.js
+++ src/views/JammingStudio/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-import { Howler } from 'howler'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { Howler } from "howler";
 
 import {
   requestRoomDetail,
@@ -11,81 +11,88 @@ import {
   triggerPause,
   triggerStop,
   clearTracks,
-  setActiveRoom,
-} from '../../store/actions/'
+  setActiveRoom
+} from "../../store/actions/";
 
-import './style.css'
+import "./style.css";
 
-import ChatRoom from '../../components/ChatRoom'
-import StudioHeader from './components/StudioHeader'
-import Track from './components/Track'
+import ChatRoom from "../../components/ChatRoom";
+import StudioHeader from "./components/StudioHeader";
+import Track from "./components/Track";
 
 export default function JammingStudio() {
-  const socket = window.socket
-  const { roomId } = useParams()
-  const dispatch = useDispatch()
+  const socket = window.socket;
+  const { roomId } = useParams();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(requestRoomDetail(roomId))
-  }, [dispatch, roomId])
+    dispatch(requestRoomDetail(roomId));
+  }, [dispatch, roomId]);
 
-  const roomDetail = useSelector((state) => state.roomReducer.activeRoom.detail)
-  const tracks = useSelector((state) => state.roomReducer.activeRoom.tracks)
-  const currentUser = useSelector((state) => state.userReducer.currentUser)
+  const roomDetail = useSelector(state => state.roomReducer.activeRoom.detail);
+  const tracks = useSelector(state => state.roomReducer.activeRoom.tracks);
+  const currentUser = useSelector(state => state.userReducer.currentUser);
 
-  const isPlaying = useSelector((state) => state.trackReducer.isPlaying)
-  const isPaused = useSelector((state) => state.trackReducer.isPaused)
-  const isStopped = useSelector((state) => state.trackReducer.isStopped)
-  const loadedTracks = useSelector((state) => state.trackReducer.loadedTracks)
+  const isPlaying = useSelector(state => state.trackReducer.isPlaying);
+  const isPaused = useSelector(state => state.trackReducer.isPaused);
+  const isStopped = useSelector(state => state.trackReducer.isStopped);
+  const loadedTracks = useSelector(state => state.trackReducer.loadedTracks);
 
   const handlePlayButton = () => {
-    dispatch(triggerPlay(!isPlaying))
-  }
+    dispatch(triggerPlay(!isPlaying));
+  };
   const handleStopButton = () => {
-    dispatch(triggerStop(!isStopped))
-  }
+    dispatch(triggerStop(!isStopped));
+  };
   useEffect(() => {
-    dispatch(requestCurrentUser())
-    socket.on('new_person_enters', (room) => {
+    dispatch(requestCurrentUser());
+
+    socket.on("new_person_enters", room => {
       if (room._id.toString() === roomId) {
-        let newRoom = { detail: room, tracks }
-        console.log(newRoom)
-        dispatch(setActiveRoom(newRoom))
+        let newRoom = { detail: room, tracks };
+        dispatch(setActiveRoom(newRoom));
       }
-      console.log(room)
-    })
+      console.log(room, "ini console.log room");
+    });
+    socket.on(`new_track_upload_${roomId}`, () => {
+      dispatch(requestRoomDetail(roomId));
+    });
     return () => {
-      Howler.unload()
-      console.log('Jamming studio unmounted')
-      dispatch(clearTracks())
-    }
-  }, [dispatch])
+      Howler.unload();
+      console.log("Jamming studio unmounted");
+      dispatch(clearTracks());
+    };
+  }, [dispatch]);
 
   const loadingBarContainerStyle = {
-    width: '200%',
-    height: '5px',
-    marginBottom: '1rem',
-    borderRadius: '3px',
-    backgroundColor: 'lavender',
-  }
+    width: "200%",
+    height: "5px",
+    marginBottom: "1rem",
+    borderRadius: "3px",
+    backgroundColor: "lavender"
+  };
   const [loadingBarProgress, setLoadingBarProgress] = useState({
-    width: '0%',
-    height: '100%',
-    borderRadius: '3px',
-    backgroundColor: 'lawngreen',
-  })
+    width: "0%",
+    height: "100%",
+    borderRadius: "3px",
+    backgroundColor: "lawngreen"
+  });
 
   return (
     <>
-      <div className='container-chatroom'>
-        <ChatRoom currentUser={currentUser} roomId={roomId} roomDetail={roomDetail} />
+      <div className="container-chatroom">
+        <ChatRoom
+          currentUser={currentUser}
+          roomId={roomId}
+          roomDetail={roomDetail}
+        />
       </div>
-      <div className='studio'>
-        <div className='studio-Container'>
-          <div className='upper'>
+      <div className="studio">
+        <div className="studio-Container">
+          <div className="upper">
             <StudioHeader roomDetail={roomDetail} />
-            <div className='studio-Main'>
-              <div className='trackContainer'>
+            <div className="studio-Main">
+              <div className="trackContainer">
                 {tracks.map((track, i) => (
                   <Track key={i} instrument track={track} />
                 ))}
@@ -94,23 +101,23 @@ export default function JammingStudio() {
           </div>
 
           {loadedTracks.length == tracks.length && loadedTracks.length !== 0 && (
-            <div className='playback-container text-white'>
-              <div className='playback-btnGroup'>
+            <div className="playback-container text-white">
+              <div className="playback-btnGroup">
                 <i
                   class={`fas fa-stop faBtn faBtnSmoll`}
                   onClick={() => {
-                    handleStopButton()
+                    handleStopButton();
                   }}
                 ></i>
                 <i
                   class={`fas faBtn playBtn ${
-                    isPlaying ? 'faBtnActive fa-pause' : 'fa-play'
+                    isPlaying ? "faBtnActive fa-pause" : "fa-play"
                   }`}
                   onClick={() => {
                     if (isPaused) {
-                      handlePlayButton(false)
+                      handlePlayButton(false);
                     } else {
-                      handlePlayButton(true)
+                      handlePlayButton(true);
                     }
                   }}
                 ></i>
@@ -120,5 +127,5 @@ export default function JammingStudio() {
         </div>
       </div>
     </>
-  )
+  );
 }
